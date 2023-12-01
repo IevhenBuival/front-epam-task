a755b94f4871d2738e3420e43dbba2987081aa22
IevhenBuival <kdotbull@gmail.com>
2023-12-01 11:44:31 +0200

    Task from workshop #6: clean imports

--- src/app/shared/components/epm-chips/epm-chips.component.ts
+++ src/app/shared/components/epm-chips/epm-chips.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { CommonModule, NgOptimizedImage } from '@angular/common';
 
 import { INTEREST_CHIPS_NAMES } from '../../../app.config';
